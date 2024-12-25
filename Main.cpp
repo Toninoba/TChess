51f02c17b41a007943890879f4f72282573e5e2d
Toninoba <tobiaspetsch1401@gmail.com>
2024-12-25 22:52:45 +0100

    Added Project Foundation

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Tobi on 25.12.2024.
+//
+
+#include "iostream"
+#include <SDL.h>
+
+
+int main(int argc, char* argv[]){
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Quit();
+    return 0;
+
+}
